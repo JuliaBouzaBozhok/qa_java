a7ce05cbd5eb31c4fe4003fa34913dca20b29bb3
Julia Bouza <julia.jmpr@yandex.ru>
2024-02-04 20:01:26 +0300

    added branches

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,26 @@
+import com.example.Feline;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+    //Тесты
+    @Test
+    public void eatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedGetFood = Arrays.asList("Животные", "Птицы", "Рыба");
+        List<String> actualGetFood = feline.eatMeat();
+        assertEquals("Список еды не соответствует Кошачьим = Хищники", expectedGetFood, actualGetFood);
+    }
+
+    @Test
+    public void getFamily() {
+        Feline feline = new Feline();
+        String expectedGetFamily = "Кошачьи";
+        String actualGetFamily = feline.getFamily();
+        assertEquals("Семейство Кошачьи", expectedGetFamily, actualGetFamily);
+    }
+}
